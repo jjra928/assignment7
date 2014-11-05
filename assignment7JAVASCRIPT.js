4844632a7f547822fb5943854d2b49f45c435482
jjra928 <jjra928@gmail.com>
2014-11-04 19:07:46 -0500

    uploading assignment

---
+++ assignment7JAVASCRIPT.js
@@ -0,0 +1,45 @@
+function getContent(fragmentId, callback) {
+	var request = new XMLHttpRequest();
+	
+	request.onload = function() {
+		callback(request.responseText);
+	};
+	
+	request.open("GET", fragmentId + ".html");
+	request.send(null);
+}
+
+function setActiveLink(fragmentId) {
+	var navbarDiv = document.getElementById("navbar"),
+			links = navbarDiv.children,
+			i, link, pagename;
+		
+	for(i = 0; i < links.length; i++) {
+		link = links[i];
+		pageName = link.getAttribute("href").substr(1);
+		if(pageName === fragmentId) {
+			link.setAttribute("class", "active");
+		} else {
+			link.removeAttribute("class");
+		}
+	}
+}
+
+function navigate() {
+	var contentDiv = document.getElementById("content"),
+			fragmentId = location.hash.substr(1);
+	
+	getContent(fragmentId, function (content) {
+		contentDiv.innerHTML = content;
+		});
+	
+	setActiveLink(fragmentId);
+}
+
+if(!location.hash) {
+	location.hash = "#home";
+}
+
+navigate();
+
+window.addEventListener("hashchange", navigate);
