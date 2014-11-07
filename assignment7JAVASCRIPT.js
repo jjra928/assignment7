59904017de58cea999f69593f5e77f2942269631
jjra928 <jjra928@gmail.com>
2014-11-07 01:55:28 -0500

    final update hopefully

--- assignment7JAVASCRIPT.js
+++ assignment7JAVASCRIPT.js
@@ -1,3 +1,16 @@
+//File:  http://jjra928.github.io/assignment7/assignment7HTML.html
+//91.461 Assignment7 :  Creating a Single Page Navigation Interface
+//Joseph Raffa, UMass Lowell Computer Science Undergradute, jraffa@cs.uml.edu
+//Date: 11-03-2014
+//Copyright (c) 2014 by Joseph Raffa.  All rights reserved.  
+//An html file to lay out the structure for a web page that will create 
+//a little blog site for my band.   
+
+//getContent takes a fragment id, and a callback function
+//var request gets a new request for some html code
+//it loads the request (text) and sends to the callback
+//tacks on .html to the id in order to grab the right html file
+//sends request
 function getContent(fragmentId, callback) {
 	var request = new XMLHttpRequest();
 	
@@ -9,6 +22,7 @@ function getContent(fragmentId, callback) {
 	request.send(null);
 }
 
+//function that sets the active class for the link  who's page we are currently on. 
 function setActiveLink(fragmentId) {
 	var navbarDiv = document.getElementById("navbar"),
 			links = navbarDiv.children,
@@ -25,6 +39,7 @@ function setActiveLink(fragmentId) {
 	}
 }
 
+//this function uses get content to obtain text, and then sets it as the inner html of a div on the page!
 function navigate() {
 	var contentDiv = document.getElementById("content"),
 			fragmentId = location.hash.substr(1);
@@ -36,10 +51,12 @@ function navigate() {
 	setActiveLink(fragmentId);
 }
 
+//grabs the has at the end of the web address.
 if(!location.hash) {
 	location.hash = "#about";
 }
 
 navigate();
 
+//adds listener for if the hash at the end of the page changes.
 window.addEventListener("hashchange", navigate);
